985d03fc289b63b276ed044bd39575470e1e9a25
FraBona <francescobonafede08@gmail.com>
2023-10-06 15:20:06 +0200

    commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+for(i = 0; i < 100; i++){
+  let n = i + 1;
+
+  const n3 = n % 3;
+  const n5 = n % 5;
+
+  if((n3 == 0) && (n5 == 0)){
+    n = 'fizzbuzz';
+  }
+
+  if((n % 3) == 0){
+    n = 'fizz';
+  }
+
+ if((n % 5) == 0){
+    n = 'buzz';
+  }
+  
+  console.log(n);
+}
